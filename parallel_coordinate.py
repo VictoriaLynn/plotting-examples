f1ea0b6da79e62e55f60af95c956945f8a5259a9
Matthew Woodruff <mjw5407@psu.edu>
2013-08-25 22:43:28 -0400

    Now behaves like Jon's

--- parallel_coordinate.py
+++ parallel_coordinate.py
@@ -32,16 +32,46 @@ ax = fig.add_subplot(1, 1, 1)    # make axes to plot on
 
 table = pandas.read_table("example_data/column_data.txt", 
                           sep=' ', header=None) 
-
 mins = table.min()
 maxs = table.max()
 
+scaled = table.copy()
 for column in table.columns:
     mm = table[column].min()
     mx = table[column].max()
-    table[column] = (table[column] - mm) / (mx - mm)
+    scaled[column] = (table[column] - mm) / (mx - mm)
 
-print table[0:10]
-#fig.savefig("imagesc_plot.png")
+brushing = (scaled[1] > 0.2) & (scaled[3] < 0.5)
+
+cmap = matplotlib.cm.get_cmap("jet")
+
+# draw gray "brushed off" solutions
+# The xor (^) inverts the brushing
+for solution in scaled[brushing ^ True].iterrows():
+    ys = solution[1]
+    xs = range(len(ys))
+
+    ax.plot(xs, ys, "-", color=(0.85,0.85,0.85), linewidth=2)
+
+for solution in scaled[brushing].iterrows():
+    ys = solution[1]
+    xs = range(len(ys))
+
+    ax.plot(xs, ys, c=cmap(ys[0]), linewidth=2)
+
+sm = matplotlib.cm.ScalarMappable(cmap=cmap)
+sm.set_array([0.0,1.0])
+
+cbar = fig.colorbar(sm)
+
+cbar.ax.set_ylabel("Scaled Column 1 Value")
+
+ax.set_title("Parallel Coordinate Plot of Column Data")
+ax.set_ylabel("Scaled Values")
+ax.set_xticks([0,1,2,3])
+ax.set_xticklabels(["Column {0}".format(ii) for ii in range(1,5)])
+
+fig.set_figwidth(11)
+fig.savefig("parallel_coordinate.png")
 
 # vim:ts=4:sw=4:expandtab:ai:colorcolumn=68:number:fdm=indent
