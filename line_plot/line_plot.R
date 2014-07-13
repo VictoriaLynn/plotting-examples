9fd252ca9636ccb27faeb59a1ed56b1d0dc4b21c
U-Tori-HP\Tori <Tori@Tori-HP.(none)>
2014-07-12 20:43:47 -0400

    fixed line styles

--- line_plot/line_plot.R
+++ line_plot/line_plot.R
@@ -13,11 +13,11 @@ y <- cbind(M[ ,2],M[ ,3],M[ ,4])
 par(mar = c(2,2,2,2), font = 1.5)
 
 matplot(x,y,type="l",xlab="X",ylab="Y",main="Line Plot of Column Data",
-        col=c("black", "darkgray", "red"), lwd=c(2))
+        col=c("black", "red", "darkgray"), lwd=c(2), lty=c(1,2,1))
 
 grid(col = "lightgray", lty = "dotted",
      lwd = c(2))
 
 #add a legend
-legend(6.2,0.4, c("Series 1","Series 2","Series 3"), lty=c(1,1),
-       lwd=c(2,2),col=c("black","darkgray","red")) 
+legend(6.2,0.4, c("Series 1","Series 2","Series 3"), lty=c(1,2,1),
+       lwd=c(2,2),col=c("black","red","darkgray")) 
