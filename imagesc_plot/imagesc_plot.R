c6b92f8b2b1078d9e8b7608284f53fed891d15f5
Victoria Ward <vlw27@cornell.edu>
2014-07-18 16:05:09 -0400

    Imagesc plot in R

---
+++ imagesc_plot/imagesc_plot.R
@@ -0,0 +1,31 @@
+# create R version of Matlab's imagesc plot
+# of gridded 2D data
+
+# Mise en place
+rm(list = ls())
+graphics.off()
+
+#Installing and loading required packages
+if (!require("fields")) {
+  install.packages("fields", dependencies = TRUE)
+  library(animation)
+}
+
+#- Open a plotting device
+#pdf("imagesc_plot.pdf", width=7, height=5)
+
+#set working directory (necessary if not using Rstudio)
+#setwd("C:/Users/Tori/My\ Documents/plotting-examples/line_plot")
+
+#read data 
+M<- as.matrix(read.table("../example_data/gridded_2D.txt",sep=" ",header=FALSE))
+
+x <- seq(0, 1, by=0.01)
+y <- seq(0,1, by=0.01)
+
+#plotting margins.  
+par(mar = c(5,5,2,2), font = 1.5)
+
+#make the image plot
+image.plot(x,y,M, main="Imagesc Plot of Gridded 2D Data",xlab="X",ylab="Y",
+           legend.lab ="Z",col=heat.colors(40), zlim=range(M))
