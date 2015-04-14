8be25714fdb4058a0df6b286ec97ef0a8667b134
U-Tori-HP\Tori <vlw27@cornell.edu>
2015-04-13 20:39:15 -0400

    updated 3D scatter in Python

--- 3D-scatter/scatter_3D.py
+++ 3D-scatter/scatter_3D.py
@@ -13,30 +13,28 @@ from mpl_toolkits.mplot3d import Axes3D
 import pandas # data analysis library
 
 #load data
-table = pandas.read_table("./myLake4ObjStoch.reference",
+table = pandas.read_table("../example_data/five_obj_data.txt",
                           sep=' ', header=None,
-                          names=["obj1", "obj2", "obj3","obj4"])
-
-table.obj2 = -1*table.obj2
-table.obj3 = -1*table.obj3
-table.obj4 = -1*table.obj4
+                          names=["obj1", "obj2", "obj3","obj4","obj5"])
 
 fig = p.figure() # create the figure
 #agg.FigureCanvasAgg(fig) # attach the rasterizer
 ax3D = Axes3D(fig)
 #ax3D = fig.add_subplot(111, projection='3d')
 
+obj5_in_points = ax3D.transData.transform(zip([0]*len(table.obj5), table.obj5)) 
+
 cmap = pyplot.cm.get_cmap("jet")
-pts = ax3D.scatter(table.obj1, table.obj2,table.obj3,
+pts = ax3D.scatter(table.obj1, table.obj2,table.obj3, s=obj5_in_points/5,
                     c=table.obj4,cmap=cmap, linewidth=0)
 
 fig.colorbar(pts,ax=ax3D,shrink=0.75)
-fig.axes[-1].set_ylabel("Reliability")
+fig.axes[-1].set_ylabel("obj4")
 
-ax3D.set_xlabel("Phosphorus")
-ax3D.set_ylabel("Benefit")
-ax3D.set_zlabel("Inertia")
-ax3D.set_title("4 objective lake problem")
+ax3D.set_xlabel("obj1")
+ax3D.set_ylabel("obj2")
+ax3D.set_zlabel("obj3")
+ax3D.set_title("Scatter Plot of 5 Objectives")
 p.show()
 
 fig.savefig("scatter_plot_3d.png")
